fd3845fa62b1eb0aa7fbc44951b8421f58ab607e
Rolen Le <rolentle@gmail.com>
2013-11-01 13:20:31 -0600

    added tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
 class Article < ActiveRecord::Base
+  has_many :taggings
+  has_many :tags, through: :taggings
   has_many :comments
+
+  def tag_list
+    self.tags.map(&:name).join ", "
+  end
+
+  def tag_list= (tags_string)
+    tag_names = tags_string.split(',').map { |s| s.strip.downcase }.uniq
+    new_or_found_tags = tag_names.map do |name|
+      Tag.find_or_create_by(name: name)
+    end
+    self.tags = new_or_found_tags
+  end
 end
